2c8780e015274ce23cd1e9a26b49bcd12c6cd68f
Groszi01 <109898772+Groszi01@users.noreply.github.com>
2022-07-24 12:53:21 +0200

    rickroll

---
+++ rickroll.py
@@ -0,0 +1,10 @@
+#Imports webbrowser to the file
+import webbrowser
+
+#URL that the file will open(You can change it if you want to)
+rickroll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+# Opens the url in a new window of the default browser, if possible
+webbrowser.open_new(rickroll)
+
+
